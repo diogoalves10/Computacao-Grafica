afae1442591a199800c4a616faeb092fc50f11b0
diogoalves10 <a79751@alunos.uminho.pt>
2019-03-01 22:49:35 +0000

    caixa criada

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -25,3 +25,77 @@ void makePlane(double side, string file) {
 	
 	f.close();
 }
+
+
+
+void makeBox(double comprimento , double altura , double largura, string file ){
+
+	ofstream f;
+	f.open(file, std::fstream::in | std::fstream::out | std::fstream::app);
+
+	c = comprimento/2;
+	a = altura;
+	la = largura /2;
+
+	//base
+	f << "" << (c) << " 0 " << (la) << endl;
+	f << "" << (-c) << " 0 " << (la) << endl;
+	f << "" << (-c) << " 0 " << (-la) << endl;
+
+	f << "" << (c) << " 0 " << (la) << endl;
+	f << "" << (-c) << " 0 " << (-la) << endl;
+	f << "" << (c) << " 0 " << (-la) << endl;
+
+
+	//topo
+
+	f << "" << (c) << (a) << (la) << endl;
+	f << "" << (-c) << (a) << (la) << endl;
+	f << "" << (-c) << (a) << (-la) << endl;
+
+	f << "" << (c) << (a) << (la) << endl;
+	f << "" << (-c) << (a) << (-la) << endl;
+	f << "" << (c) << (a) << (-la) << endl;
+
+	//face 1
+
+	f << "" << (-c) << " 0 " << (-la) << endl;
+	f << "" << (c) << " 0 " << (-la) << endl;
+	f << "" << (c) << (a) << (-la) << endl;
+
+	f << "" << (-c) << " 0 " << (-la) << endl;
+	f << "" << (-c) << (a) << (-la) << endl;
+	f << "" << (c) << (a) << (-la) << endl;
+
+	//face 2
+
+	f << "" << (c) << " 0 " << (-la) << endl;
+	f << "" << (c) << (a) << (-la) << endl;
+	f << "" << (c) << (a) << (la) << endl;
+
+	f << "" << (c) << " 0 " << (la) << endl;
+	f << "" << (c) << (a) << (la) << endl;
+	f << "" << (c) << " 0 " << (-la) << endl;
+
+	//face 3
+	f << "" << (c) << (a) << (la) << endl;
+	f << "" << (c) << " 0 " << (la) << endl;
+	f << "" << (-c) << " 0 " << (la) << endl;
+
+	f << "" << (-c) << " 0 " << (la) << endl;
+	f << "" << (-c) << (a) << (la) << endl;
+	f << "" << (c) << (a) << (la) << endl;
+
+	//face 4
+	f << "" << (-c) << " 0 " << (la) << endl;
+	f << "" << (-c) << (a) << (la) << endl;
+	f << "" << (-c) << " 0 " << (-la) << endl;
+
+	f << "" << (-c) << " 0 " << (-la) << endl;
+	f << "" << (-c) << (a) << (-la) << endl;
+	f << "" << (-c) << (a) << (la) << endl;
+
+	f.close();
+
+
+}
